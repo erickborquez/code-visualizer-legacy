8479fcfe9aa88c4d1dc8ecd7c55d0374da7ed176
ErickJoestar <31946895+ErickJoestar@users.noreply.github.com>
2019-09-26 23:37:11 -0500

    Added some minor changes

--- src/App.js
+++ src/App.js
@@ -10,20 +10,34 @@ import build from './Components/Build';
 function App() {
 
 
-  const [code, setCode] = useState('let array = new Array1D();\nlet history = new algorithmCanvas();');
-  const [steps, setSteps] = useState([]);
+  const [code, setCode] = useState(`
+  let array = new Array1D([1, 3, 4, 5], 'array1');
+  let array2 = new Array1D([1, 3, 4, 5], 'array2');
+  
+  let records = new AlgorithmCanvas();
+  
+  array.highlight({ element: 1, color: 'red' }, { elements: [0, 5], color: 'green' })
+  
+  records.watch(array, array2);
+  records.draw();
+  records.draw();
+  records.end();
+  
+  `);
+
+
+  const [records, setRecords] = useState([]);
+  const [actualStep, setStep] = useState(0);
 
   useEffect(() => {
     const keyPress = (e) => {
       if (e.ctrlKey && e.key === 'Enter') {
-        build(code, setSteps);
+        build(code, setRecords, setStep);
       }
     }
-
     window.addEventListener("keydown", keyPress);
     return () => {
       window.removeEventListener("keydown", keyPress);
-
     }
   })
 
@@ -31,8 +45,9 @@ function App() {
   return (
     <div className="App">
       <CodeEditor code={code} onCodeChange={(code) => setCode(code)} />
-      <button onClick={() => build(code, setSteps)}>Build!</button>
-      <Visualizer steps={steps} />
+      <button onClick={() => build(code, setRecords, setStep)}>Build!</button>
+      <button onClick={() => setStep(actualStep + 1)}>New step bb</button>
+      <Visualizer records={records} step={actualStep % (records.length)} />
     </div>
   );
 }
--- src/codigoPrueba.js
+++ src/codigoPrueba.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-undef */
 
-let array = new Array1D([1, 3, 4, 5]);
-let history = new AlgorithmCanvas();
+let array = new Array1D([1, 3, 4, 5], 'array1');
+let array2 = new Array1D([1, 3, 4, 5], 'array2');
+
+let records = new AlgorithmCanvas();
 
 array.highlight({ element: 1, color: 'red' }, { elements: [0, 5], color: 'green' })
 
-history.watch(array, array);
-history.draw();
-console.log(history.steps[0].changes[1]);
+records.watch(array, array2);
+records.draw();
+records.draw();
+records.end();
