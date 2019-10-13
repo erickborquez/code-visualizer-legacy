bb51d6363f76ce53a889ebd6634350288c699bd0
ErickJoestar <31946895+ErickJoestar@users.noreply.github.com>
2019-10-13 02:06:11 -0500

    Added components to sign in and out

---
+++ src/Components/SignInAndOut.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { provider, signInWithGoogle, signOut } from '../firebase'
+const SignInAndOut = () => {
+    return (
+        <div>
+            <button onClick={() => signInWithGoogle()} >Sign In!!!</button>
+            <button onClick={() => signOut()}>Sign Out!!!</button>
+        </div>
+    )
+}
+
+export default SignInAndOut;
